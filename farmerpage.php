7c73249e7e9962669678a467635912b2ff2b7ea6
Roniel <ronielmclaindacillo@gmail.com>
2025-05-01 23:31:08 +0800

    Fuggg

--- farmerpage.php
+++ farmerpage.php
@@ -258,22 +258,22 @@ $pending_orders = 5;
 
         <nav class="mt-6">
             <ul class="space-y-2">
-                <li><a href="#" class="flex items-center justify-center p-2 bg-[#CAEED5] text-green-700 rounded">
+                <li><a href="#" class="flex items-center  p-2 bg-[#CAEED5] text-green-700 rounded">
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
                     </svg>
                     Home</a></li>
-                <li><a href="farmerprofile.php" class="flex items-center justify-center p-2 hover:bg-[#CAEED5] hover:text-green-700 rounded">
+                <li><a href="farmerprofile.php" class="flex items-center  p-2 hover:bg-[#CAEED5] hover:text-green-700 rounded">
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                     </svg>
                     Profile</a></li>
-                <li><a href="#" class="flex items-center justify-center p-2 hover:bg-[#CAEED5] hover:text-green-700 rounded">
+                <li><a href="#" class="flex items-center  p-2 hover:bg-[#CAEED5] hover:text-green-700 rounded">
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
                     </svg>
                     Notifications</a></li>
-                <li><a href="#" class="flex items-center justify-center p-2 text-red-500 hover:text-red-700" onclick="openLogoutModal()">
+                <li><a href="#" class="flex items-center  p-2 text-red-500 hover:text-red-700" onclick="openLogoutModal()">
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
                     </svg>
