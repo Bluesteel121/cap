2f069bc0a6c0fc3cba9afa5bfe2732fdfa22dcf0
RonielDacillo <ronielmclaindacillo@gmail.com>
2025-05-16 23:18:23 +0800

    NIgga Dog

--- adminpage.php
+++ adminpage.php
@@ -23,17 +23,19 @@
             
             <nav class="mt-6 px-3 flex-grow">
                 <ul class="space-y-1">
+                     <li><a href="#" class="flex items-center p-2 rounded bg-[#CAEED5] text-green-800">
+                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
+                        </svg>
+                        Home</a>
+
                     <li><a href="#" class="flex items-center p-2 rounded hover:bg-[#115D5B]">
                         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
                         </svg>
-                        Home</a>
+                        Request</a>
                     </li>
-                    <li><a href="#" class="flex items-center p-2 rounded bg-[#CAEED5] text-green-800">
-                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
-                        </svg>
-                        Order</a>
+                   
                     </li>
                     <li><a href="#" class="flex items-center p-2 rounded hover:bg-[#115D5B]">
                         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
