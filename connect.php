192f045cf10cd56d5c9146ba105f6029d205a863
RonielDacillo <ronielmclaindacillo@gmail.com>
2025-05-18 22:39:11 +0800

    prod connection for connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,12 @@
 <?php
-$servername = "localhost";  
-$username = "root";         
-$password = "";             
-$database = "capstone"; 
+// $servername = "localhost";  
+// $username = "root";         
+// $password = "";             
+// $database = "capstone"; 
+$conn = new mysqli("mysql-database-i4g8gso0goc8cws8cocc4ks0", "root", "uz9Fb2ZvJVlwLYYoGwTieloCHFY0Yv3uqN9XUDkDJlHy8QZL4x6jdZQDOvW3cZDV", "capstone"); //prod connection
 
 
-$conn = new mysqli($servername, $username, $password, $database);
+// $conn = new mysqli($servername, $username, $password, $database);
 
 
 if ($conn->connect_error) {
