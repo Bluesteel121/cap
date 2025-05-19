0b50c38e0d05551d17d6b63e4384a624c451f5d9
RonielDacillo <ronielmclaindacillo@gmail.com>
2025-05-20 03:03:01 +0800

    Button on ourservices is fixed

--- services.php
+++ services.php
@@ -25,12 +25,12 @@
       <!-- Navigation Menu -->
       <nav id="nav-menu" class="hidden xl:block">
         <ul class="flex flex-col xl:flex-row xl:space-x-6 bg-[#115D5B] xl:bg-transparent xl:static absolute left-0 top-full w-full xl:w-auto p-4 xl:p-0 shadow-md xl:shadow-none">
-          <li><a href="index.html" class="text-white xl:text-black font-bold hover:text-gray-300 block py-2">Home</a></li>
+          <li><a href="index.php" class="text-white xl:text-black font-bold hover:text-gray-300 block py-2">Home</a></li>
           <li><a href="services.php" class="text-white xl:text-black font-bold hover:text-gray-300 block py-2">Our Services</a></li>
           <li><a href="#contact" class="text-white xl:text-black font-bold hover:text-gray-300 block py-2">Contact Us</a></li>
           <li><a href="#faq" class="text-white xl:text-black font-bold hover:text-gray-300 block py-2">FAQ</a></li>
-          <li><a href="elibrary.html" class="text-white xl:text-black font-bold bg-green-1000 px-4 py-2 rounded-lg hover:text-gray-300 block py-2">Library</a></li>
-          <li><a href="account.html" class="text-white xl:text-black font-bold bg-green-900 px-6 py-2 rounded-lg hover:text-gray-300 block py-2">Log In</a></li>
+          <li><a href="elibrary.php" class="text-white xl:text-black font-bold bg-green-1000 px-4 py-2 rounded-lg hover:text-gray-300 block py-2">Library</a></li>
+          <li><a href="account.php" class="text-white xl:text-black font-bold bg-green-900 px-6 py-2 rounded-lg hover:text-gray-300 block py-2">Log In</a></li>
         </ul>
       </nav>
     </div>
