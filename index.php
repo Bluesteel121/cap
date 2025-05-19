ff699a4c3c2943f7ae563f10130fcf058b62a693
RonielDacillo <ronielmclaindacillo@gmail.com>
2025-05-20 02:48:43 +0800

    Hamburger fixed

--- index.php
+++ index.php
@@ -25,12 +25,12 @@
     <!-- Navigation Menu -->
     <nav id="nav-menu" class="hidden xl:block">
       <ul class="flex flex-col xl:flex-row xl:space-x-6 bg-[#115D5B] xl:bg-transparent xl:static absolute left-0 top-full w-full xl:w-auto p-4 xl:p-0 shadow-md xl:shadow-none">
-        <li><a href="#" class="hover:text-gray-300 block py-2">Home</a></li>
-        <li><a href="#" class="hover:text-gray-300 block py-2">Our Services</a></li>
-        <li><a href="#contact" class="hover:text-gray-300 block py-2">Contact Us</a></li>
-        <li><a href="#faq" class="hover:text-gray-300 block py-2">FAQ</a></li>
-        <li><a href="elibrary.php" class="bg-green-1000 px-4 py-2 rounded-lg hover:text-gray-300 block py-2">Library</a></li>
-        <li><a href="account.php" class="bg-green-900 px-6 py-2 rounded-lg hover:text-gray-300 block py-2">Log In</a></li>
+        <li><a href="index.html" class="text-white xl:text-black font-bold hover:text-gray-300 block py-2">Home</a></li>
+        <li><a href="services.php" class="text-white xl:text-black font-bold hover:text-gray-300 block py-2">Our Services</a></li>
+        <li><a href="#contact" class="text-white xl:text-black font-bold hover:text-gray-300 block py-2">Contact Us</a></li>
+        <li><a href="#faq" class="text-white xl:text-black font-bold hover:text-gray-300 block py-2">FAQ</a></li>
+        <li><a href="elibrary.php" class="text-white xl:text-black font-bold bg-green-1000 px-4 py-2 rounded-lg hover:text-gray-300 block py-2">Library</a></li>
+        <li><a href="account.html" class="text-white xl:text-black font-bold bg-green-900 px-6 py-2 rounded-lg hover:text-gray-300 block py-2">Log In</a></li>
       </ul>
     </nav>
   </div>
@@ -42,7 +42,7 @@
     <div class="absolute inset-0 flex flex-col items-center justify-center text-center text-white bg-black bg-opacity-50">
       <h2 class="text-3xl font-bold">Welcome to Camarines Norte Lowland Rainfed Research Station</h2>
       <p class="mt-2 text-lg">Learn and Discover the Secrets Behind the Sweetest Pineapple in Camarines Norte</p>
-      <a href="#" class="mt-4 bg-green-500 px-4 py-2 rounded-md text-white font-semibold hover:bg-green-700">Explore More</a>
+      <a href="about.html" class="mt-4 bg-green-500 px-4 py-2 rounded-md text-white font-semibold hover:bg-green-700">Explore More</a>
     </div>
   </section>
 
